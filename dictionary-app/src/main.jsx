9d07acdc9cd74b4ead234851ad8f6018733282e1
Amirreza938 <120088072+Amirreza938@users.noreply.github.com>
2025-05-04 17:59:24 +0430

    fix: deployment

--- dictionary-app/src/main.jsx
+++ dictionary-app/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'  // Update path to point to the styles directory
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
